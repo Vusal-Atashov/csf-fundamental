562f238d2f826c23c92809afaf7e50784f6c9381
macbook <vusalatashov@gmail.com>
2024-01-24 13:13:03 +0400

    init task1App.java

---
+++ EolympTasks/EolympTasks/src/main/java/az/edu/turing/task4App.java
@@ -0,0 +1,19 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class task4App {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+        long prod = 1;
+        while (num > 0) {
+            int remainder = num % 10;
+            prod *= remainder;
+            num /= 10;
+        }
+        System.out.println(prod);
+    }
+}
+
+
+
