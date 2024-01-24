937be499584b7bc2c277795fa4368f8ab162dae8
macbook <vusalatashov@gmail.com>
2024-01-24 12:23:15 +0400

    init task1App.java

---
+++ EolympTasks/EolympTasks/src/main/java/az/edu/turing/task2App.java
@@ -0,0 +1,18 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+
+public class task2App {
+    public static void main(String[] args) {
+        int level = new Scanner(System.in).nextInt();
+        if (level >= 1 && level <= 3) {
+            System.out.println("Initial");
+        } else if (level <= 6) {
+            System.out.println("Average");
+        } else if (level <= 9) {
+            System.out.println("Sufficient");
+        } else if (level <= 12) {
+            System.out.println("High");
+        }
+    }
+}
