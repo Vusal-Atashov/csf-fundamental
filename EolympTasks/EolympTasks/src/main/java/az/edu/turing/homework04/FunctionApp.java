d66bc82977cd1ccf1ee6b4bed4756c243faa5f6b
macbook <vusalatashov@gmail.com>
2024-02-09 01:45:54 +0400

    init eolymp

--- EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
+++ EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
@@ -2,11 +2,14 @@ package az.edu.turing.homework04;
 
 import java.util.Scanner;
 
-public class FunctionApp {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        double y =(((2*x)-1)/(Math.pow(x,2))+((Math.sqrt(Math.pow(x,2)+1))/2));
-        System.out.printf("%.3f", y);
+public class FunctionApp
+{
+    public static void main(String[] args)
+    {
+        Scanner con = new Scanner(System.in);
+        double x = con.nextDouble();
+        double y = (x * x + 3 * x - 4) / (2 * x - 3)-(x + 2) / (x * x - 5 * x + 7);
+        System.out.printf("%.3f",y);
     }
 }
+
