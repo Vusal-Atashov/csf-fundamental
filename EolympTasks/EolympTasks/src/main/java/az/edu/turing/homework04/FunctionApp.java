776c5e28f2828aa6869309f77639965b8221e791
macbook <vusalatashov@gmail.com>
2024-02-09 02:14:13 +0400

    init eolymp

--- EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
+++ EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
@@ -2,14 +2,20 @@ package az.edu.turing.homework04;
 
 import java.util.Scanner;
 
-public class FunctionApp
-{
-    public static void main(String[] args)
-    {
+public class FunctionApp {
+    public static void main(String[] args) {
         Scanner con = new Scanner(System.in);
-        int x = con.nextInt();
-        int y=con.nextInt();
-        System.out.print((int)Math.pow(x,y));
+        long k = con.nextInt();
+        long sum = 0;
+        long n = 1;
+        while (true) {
+            sum += n;
+            if (sum % k == 0) {
+                System.out.println(n);
+                break;
+            }
+            n++;
+        }
     }
 }
 
