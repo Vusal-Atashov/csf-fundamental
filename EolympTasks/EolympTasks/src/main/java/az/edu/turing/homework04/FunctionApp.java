0229f50dc8fa2a18f686416e276e80b92ca04ca5
macbook <vusalatashov@gmail.com>
2024-02-09 13:01:17 +0400

    init eolymp

--- EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
+++ EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
@@ -3,13 +3,13 @@ package az.edu.turing.homework04;
 import java.util.Scanner;
 
 public class FunctionApp {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long x = scanner.nextInt();
-        long y = scanner.nextInt();
-        long z = scanner.nextInt();
-        long f =((x * y * z) + x + (y*y) + (z*z*z));
-        System.out.println(f);
-    }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            long a=scanner.nextInt();
+            System.out.printf("%.6f",Math.sqrt(a));
+        }
+
 }
 
