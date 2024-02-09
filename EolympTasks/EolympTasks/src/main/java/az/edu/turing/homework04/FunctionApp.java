374d73bfacfb47bad06887120ecd850e1153b2fd
macbook <vusalatashov@gmail.com>
2024-02-10 01:05:00 +0400

    init eolymp

--- EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
+++ EolympTasks/EolympTasks/src/main/java/az/edu/turing/homework04/FunctionApp.java
@@ -7,15 +7,9 @@ public class FunctionApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        long n = scanner.nextLong();
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-
-                System.out.print((long) Math.pow(i, 2)+" ");
-
-        }
-
-
-        scanner.close();
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        System.out.println((int)Math.pow(2,a)+(int)Math.pow(2,b));
     }
 
 }
